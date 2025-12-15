7efcddb64e4d5fc2b350cddbaa5cd6c4acbcd030
Abhinav <abhinavpandey56393@gmail.com>
2025-12-15 14:38:01 +0530

    sucessfully fetching data from api

--- finance-dataset/scripts/fetch_api.py
+++ finance-dataset/scripts/fetch_api.py
@@ -2,6 +2,7 @@ import os
 import requests
 import pandas as pd
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
@@ -9,20 +10,81 @@ API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
 
 BASE_URL = "https://www.alphavantage.co/query"
 
-params = {
-    "function": "TIME_SERIES_DAILY",
-    "symbol": "AAPL",
-    "apikey": API_KEY
-}
+final_data = []
+TOP_20_SYMBOLS = [
+    "AAPL", "MSFT", "GOOGL", "AMZN", "META",
+    "TSLA", "NVDA", "BRK.B", "JPM", "JNJ",
+    "V", "PG", "UNH", "HD", "MA",
+    "DIS", "BAC", "XOM", "NFLX", "ADBE"
+]
 
-response = requests.get(BASE_URL, params=params)
 
-data = response.json()
-df = pd.DataFrame(data['Time Series (Daily)'])
+for symbol in TOP_20_SYMBOLS:
+    params = {
+        "function":"TIME_SERIES_DAILY",
+        "symbol":symbol,
+        "apikey":API_KEY,
+        "outputsize":"compact"
+    }
+    
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+    
+    if "Time Series (Daily)" not in data:
+      print(f"API error / limit hit for {symbol}")
+      continue
 
-df = df.T
-df.shape
-df.index.name = 'date'
-df.columns = ["open", "high", "low", "close", "volume"]
-df = df.reset_index()
-print(df)
+    symbol_data = data["Time Series (Daily)"]
+    
+    if symbol_data is None:
+        print(f"No data for {symbol}")
+        continue
+    else:
+        df = pd.DataFrame(symbol_data).T
+        df.columns = ["open", "high", "low", "close", "volume"]
+        df.index.name = 'date'
+        df = df.reset_index()
+        df["symbol"] = symbol
+        df = df[["date", "symbol", "open", "high", "low", "close", "volume"]]
+        
+        df['date'] = pd.to_datetime(df['date'])
+        df[["open", "high", "low", "close", "volume"]] = df[["open", "high", "low", "close", "volume"]].apply(pd.to_numeric)
+        df = df.sort_values(by="date")
+        df = df.drop_duplicates(subset=['date','symbol'], keep='last') 
+        df = df.reset_index(drop=True)
+        final_data.append(df)
+        time.sleep(12)
+
+
+final_df = pd.concat(final_data, ignore_index=True)
+os.makedirs("finance-dataset/data/processed", exist_ok=True)
+
+final_df.to_csv(
+      "finance-dataset/data/processed/top_20_stocks.csv",
+    index=False
+)
+print(final_df.head())
+print(final_df.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
